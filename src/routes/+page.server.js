9488da7464bcfc52f85e0a8ddb4cdca5d646b56a
Riley <riley.childs@yahoo.com>
2022-11-14 19:44:04 -0500

    Deleted +page.server.js

--- src/routes/+page.server.js
+++
@@ -1,9 +0,0 @@
-// import { destinations } from '$db/collections';
-
-// export const load = async () => {
-// 	const data = await destinations.find({}).toArray();
-// 	console.log(data);
-// 	return {
-// 		destinations: JSON.parse(JSON.stringify(data))
-// 	};
-// };
