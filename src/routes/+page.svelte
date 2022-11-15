916f292d5cc918be452244b0bdbf7bc0ab2e6a7a
Riley <riley.childs@yahoo.com>
2022-11-14 19:42:50 -0500

    canged text content to home content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,14 @@
 
 <nav class="nav">00 Home 01 Destination 02 Crew 03 Technology</nav>
 
-<main class="text-content">
-	<div class="text-content__heading">
-		<h3 class="text-content__heading text-content__heading--secondary">
+<main class="home-content">
+	<div class="home-content__heading">
+		<h3 class="home-content__heading home-content__heading--secondary">
 			So, you want to travel to
 		</h3>
-		<h1 class="text-content__heading text-content__heading--primary">Space</h1>
+		<h1 class="home-content__heading home-content__heading--primary">Space</h1>
 	</div>
-	<p class="text-content__main-text">
+	<p class="home-content__main-text">
 		Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not
 		hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of
 		this world experience! Explore
