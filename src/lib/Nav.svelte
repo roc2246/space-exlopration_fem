bdcb5a190e35144e826d1a7b811bdf53dc52e332
Riley <riley.childs@yahoo.com>
2022-11-15 12:40:31 -0500

    Data is now prefetched

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,17 +1,17 @@
 <nav class="nav">
-	<a href="/" class="nav__link">
+	<a data-sveltekit-prefetch href="/" class="nav__link">
 		<span class="nav__link nav__link--no">00</span>
 		<span class="nav__link nav__link--location">Home</span>
 	</a>
-	<a href="/destination" class="nav__link">
+	<a data-sveltekit-prefetch href="/destination" class="nav__link">
 		<span class="nav__link nav__link--no">01</span>
 		<span class="nav__link nav__link--location">Destination</span>
 	</a>
-	<a href="/crew" class="nav__link">
+	<a data-sveltekit-prefetch href="/crew" class="nav__link">
 		<span class="nav__link nav__link--no">02</span>
 		<span class="nav__link nav__link--location">Crew</span>
 	</a>
-	<a href="/technology" class="nav__link">
+	<a data-sveltekit-prefetch href="/technology" class="nav__link">
 		<span class="nav__link nav__link--no">03</span>
 		<span class="nav__link nav__link--location">Technology</span>
 	</a>
