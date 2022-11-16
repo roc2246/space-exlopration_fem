fbee34a72a738203ffe7794f04f3dda9d7d7fb58
Riley <riley.childs@yahoo.com>
2022-11-16 17:08:52 -0500

    Made TopRow layout responsive

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,34 +1,72 @@
+<script>
+	let toggleSrc = './assets/shared/icon-hamburger.svg';
+</script>
+
 <nav class="nav">
+	<div class="nav__mobile-toggle">
+		<img src={toggleSrc} alt="mobile-toggle" />
+	</div>
 	<a data-sveltekit-prefetch href="/" class="nav__link">
-		<span class="nav__link--no">00</span>
+		<span class="nav__link--no">00&nbsp;</span>
 		<span class="nav__link--location">Home</span>
 	</a>
 	<a data-sveltekit-prefetch href="/destination" class="nav__link">
-		<span class="nav__link--no">01</span>
+		<span class="nav__link--no">01&nbsp;</span>
 		<span class="nav__link--location">Destination</span>
 	</a>
 	<a data-sveltekit-prefetch href="/crew" class="nav__link">
-		<span class="nav__link--no">02</span>
+		<span class="nav__link--no">02&nbsp;</span>
 		<span class="nav__link--location">Crew</span>
 	</a>
 	<a data-sveltekit-prefetch href="/technology" class="nav__link">
-		<span class="nav__link--no">03</span>
+		<span class="nav__link--no">03&nbsp;</span>
 		<span class="nav__link--location">Technology</span>
 	</a>
 </nav>
 
 <style lang="scss">
-    @import "../styles/global.scss";
+	@import '../styles/global.scss';
 	.nav {
 		display: flex;
 		flex-direction: row;
+		justify-content: center;
+		flex-wrap: wrap;
+
 		border: solid;
+		z-index: 0;
+		width: 50%;
+		height: 5rem;
+		margin-right: -2rem;
+		&__mobile-toggle {
+			display: none;
+		}
 		&__link {
 			@include centerChild();
 
 			text-decoration: none;
-			margin-left: 0.25rem;
-			margin-right: 0.25rem;
+			margin-left: 1rem;
+			margin-right: 1rem;
+		}
+	}
+
+	@media (max-width: $tablet) {
+		.nav {
+			width: 75%;
+		}
+	}
+
+	@media (max-width: $mobile) {
+		.nav {
+			@include centerChild();
+			width: auto;
+			margin-right: 0;
+			border: none;
+			&__mobile-toggle {
+				display: inline-block;
+			}
+			&__link {
+				display: none;
+			}
 		}
 	}
 </style>
--- src/lib/TopRow.svelte
+++ src/lib/TopRow.svelte
@@ -3,30 +3,40 @@
 </script>
 
 <section class="top-row">
-    <div class="top-row__img">
-        <img src="./assets/shared/logo.svg" alt="">
-    </div>
-    <div class="top-row__line">
-        <hr>
-    </div>
+	<div class="top-row__img">
+		<img src="./assets/shared/logo.svg" alt="" />
+	</div>
+	<div class="top-row__line">
+		<hr />
+	</div>
 	<Nav />
 </section>
 
 <style lang="scss">
-    @import "../styles/global.scss";
-    .top-row{
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        
+	@import '../styles/global.scss';
+	.top-row {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
 
-        &__line{
-        @include centerChild();
-
-            width: 20rem;
-            & > hr {
-                width: 100%;
-                height: .01rem;
+		&__img {
+			@include centerChild();
+		}
+		&__line {
+			@include centerChild();
+			z-index: 1;
+			width: 45%;
+			margin-right: -8%;
+			& > hr {
+				width: 100%;
+				height: 0.01rem;
+			}
+		}
+	}
+    @media(max-width: $tablet) {
+        .top-row{
+            &__line{
+                display: none;
             }
         }
     }
