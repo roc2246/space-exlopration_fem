2862a8cc2a0ade7d2ae6065fde8e964a2bcbbf18
Riley <riley.childs@yahoo.com>
2022-11-16 13:47:13 -0500

    Removed console log from each api endpoint

--- src/routes/crew/+server.js
+++ src/routes/crew/+server.js
@@ -2,7 +2,6 @@ import { crew } from '$db/collections';
 
 export const GET = async () => {
 	const data = await crew.find().toArray();
-	console.log(data);
 	return new Response(JSON.stringify(data), { status: 200 });
 };
 
--- src/routes/technology/+server.js
+++ src/routes/technology/+server.js
@@ -2,6 +2,5 @@ import { technology } from '$db/collections';
 
 export const GET = async () => {
 	const data = await technology.find().toArray();
-	console.log(data);
 	return new Response(JSON.stringify( data ), { status: 200 });
 };
