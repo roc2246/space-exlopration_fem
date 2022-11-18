16922a65cdd8bcc4b7577dc2502ba0ca38808809
Riley <riley.childs@yahoo.com>
2022-11-18 07:53:36 -0500

    Home layout is now mobile responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,8 @@
 <section class="page-container" id="home-container">
 	<TopRow />
 	<main class="home-content">
-		<div class="home-content__heading">
-			<h3 class="home-content__heading--secondary">So, you want to travel to</h3>
-			<h1 class="home-content__heading--primary">Space</h1>
-		</div>
+		<h3 class="home-content__secondary-heading">So, you want to travel to</h3>
+		<h1 class="home-content__primary-heading">Space</h1>
 		<p class="home-content__main-text">
 			Let’s face it; if you want to go to space, you might as well genuinely go to outer space and
 			not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly
@@ -25,22 +23,58 @@
 
 	.home-content {
 		display: grid;
-		grid-template-columns: max(15rem, 25%) max(2rem, 25%) auto;
+		grid-template-columns: max(15rem, 25%) max(2rem, 50%) 1fr;
 		margin-left: 2rem;
 		margin-right: 2rem;
-
+		margin-top: 10rem;
+		&__secondary-heading {
+			grid-row: 1;
+		}
+		&__primary-heading {
+			grid-row-start: 2;
+			grid-row-end: 3;
+		}
 		&__main-text {
-			grid-row: 2;
+			grid-row-start: 3;
+			grid-row-end: 4;
 		}
 		&__explore {
-			grid-column: 3;
-			grid-row: 2;
+			grid-column-start: 3;
+			grid-column-end: 4;
+			grid-row-start: 2;
+			grid-row-end: 4;
 
-			width: 20rem;
-			height: auto;
+			border-radius: 90%;
+			width: 10rem;
+			height: 10rem;
 			border: solid;
 
+			margin-top: 1rem;
+			margin-bottom: 1rem;
+
 			@include centerChild();
+		}
+	}
+
+	@media (max-width: $tablet) {
+		.home-content {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			margin-top: 5rem;
+
+			&__main-text{
+				width:25rem;
+			}
+		}
+	}
+	@media (max-width: $mobile) {
+		.home-content {
+			margin-top: .25rem;
+		
+			&__main-text{
+				width: 20rem;
+			}
 		}
 	}
 </style>
