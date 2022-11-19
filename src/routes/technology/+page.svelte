ddadcf2dd169c665846570d0d34d83fa736e7084
Riley <riley.childs@yahoo.com>
2022-11-19 06:02:39 -0500

    Finished created technology desktop layout

--- src/routes/technology/+page.svelte
+++ src/routes/technology/+page.svelte
@@ -6,7 +6,7 @@
 	let no = 0;
 </script>
 
-<section class="page-container"  id="technology-container">
+<section class="page-container" id="technology-container">
 	<TopRow />
 	<main class="technology main">
 		<h2 class="technology__title">
@@ -15,9 +15,11 @@
 		</h2>
 		<section class="technology__select">
 			{#each technology as item, i}
-				<h1 on:keydown on:click={() => (no = i)} class="technology__select--no">
-					{i}
-				</h1>
+				<div on:keydown on:click={() => (no = i)} class="technology__select--container">
+					<h1>
+						{i + 1}
+					</h1>
+				</div>
 			{/each}
 		</section>
 		<section class="technology__heading">
@@ -30,12 +32,12 @@
 		<section class="technology__image">
 			<img
 				class="technology__image--desktop"
-				src={technology[no].images.landscape}
+				src={technology[no].images.portrait}
 				alt={technology[no].name}
 			/>
 			<img
 				class="technology__image--mobile"
-				src={technology[no].images.portrait}
+				src={technology[no].images.landscape}
 				alt={technology[no].name}
 			/>
 		</section>
@@ -43,12 +45,67 @@
 </section>
 
 <style lang="scss">
-	.technology__image--desktop, .technology__image--mobile{
+	@import '../../styles/global.scss';
+
+	.technology__image--desktop,
+	.technology__image--mobile {
 		max-width: 100%;
+		height: auto;
 	}
-	.technology{
-		&__image{
-			&--mobile{
+
+	#technology-container{
+		padding-right: 0;
+	}
+
+	.technology {
+		display: grid;
+		grid-template-columns: 4rem auto auto;
+		margin-right: 0;
+		&__title {
+			grid-row: 1;
+			grid-column-start: 1;
+			grid-column-end: 3;
+		}
+
+		&__select {
+			grid-column: 1;
+			grid-row-start: 3;
+			grid-row-end: 5;
+
+			display: flex;
+			flex-direction: column;
+			gap: 2rem;
+
+			&--container {
+				border: solid;
+				border-radius: 50%;
+				width: 3.5rem;
+				height: auto;
+
+				@include centerChild();
+			}
+		}
+		&__heading, &__description {
+			margin-left: 2rem;
+		}
+		&__heading {
+			grid-row: 3;
+			// &--primary{
+			// 	margin-bottom: 0;
+			// }
+		}
+		&__description {
+			grid-column: 2;
+			grid-row: 4;
+			max-width: 22rem;
+		}
+		&__image {
+			grid-column: 3;
+			grid-row-start: 2;
+			grid-row-end: 6;
+			margin-left: auto;
+			margin-right: -2rem;
+			&--mobile {
 				display: none;
 			}
 		}
