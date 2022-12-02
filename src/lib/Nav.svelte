9b2b8bff2009c75d2b63258f4d5424c011f9a3ce
Riley <riley.childs@yahoo.com>
2022-12-01 20:00:42 -0500

    Fixed 'document not fount' error in mobbile nav toggle

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,14 +1,6 @@
 <script>
 	let toggleSrc = 'hamburger';
 
-	const mobileToggle = (display) => {
-		const links = document.getElementsByClassName('nav__link');
-		for (let x = 0; x < links.length; x++) {
-			links[x].style.display = display;
-		}
-	};
-
-	$: toggleSrc === 'close' ? mobileToggle('flex') : mobileToggle('none');
 </script>
 
 <nav class="nav">
@@ -19,22 +11,42 @@
 	>
 		<img src="./assets/shared/icon-{toggleSrc}.svg" alt="mobile-toggle" />
 	</div>
-	<a data-sveltekit-prefetch href="/" class="nav__link">
+	<!-- Desktop Links -->
+	<a data-sveltekit-prefetch href="/" class="nav__link nav__link--desktop">
 		<span class="nav__link--no">00&nbsp;</span>
 		<span class="nav__link--location">Home</span>
 	</a>
-	<a data-sveltekit-prefetch href="/destination" class="nav__link">
+	<a data-sveltekit-prefetch href="/destination" class="nav__link nav__link--desktop">
 		<span class="nav__link--no">01&nbsp;</span>
 		<span class="nav__link--location">Destination</span>
 	</a>
-	<a data-sveltekit-prefetch href="/crew" class="nav__link">
+	<a data-sveltekit-prefetch href="/crew" class="nav__link nav__link--desktop">
 		<span class="nav__link--no">02&nbsp;</span>
 		<span class="nav__link--location">Crew</span>
 	</a>
-	<a data-sveltekit-prefetch href="/technology" class="nav__link">
+	<a data-sveltekit-prefetch href="/technology" class="nav__link nav__link--desktop">
 		<span class="nav__link--no">03&nbsp;</span>
 		<span class="nav__link--location">Technology</span>
 	</a>
+	<!-- Mobile Links -->
+	{#if toggleSrc === 'close'}
+	<a data-sveltekit-prefetch href="/" class="nav__link nav__link--mobile">
+		<span class="nav__link--no">00&nbsp;</span>
+		<span class="nav__link--location">Home</span>
+	</a>
+	<a data-sveltekit-prefetch href="/destination" class="nav__link nav__link--mobile">
+		<span class="nav__link--no">01&nbsp;</span>
+		<span class="nav__link--location">Destination</span>
+	</a>
+	<a data-sveltekit-prefetch href="/crew" class="nav__link nav__link--mobile">
+		<span class="nav__link--no">02&nbsp;</span>
+		<span class="nav__link--location">Crew</span>
+	</a>
+	<a data-sveltekit-prefetch href="/technology" class="nav__link nav__link--mobile">
+		<span class="nav__link--no">03&nbsp;</span>
+		<span class="nav__link--location">Technology</span>
+	</a>
+	{/if}
 </nav>
 
 <style lang="scss">
@@ -63,6 +75,8 @@
 			&:hover {
 				border-bottom: solid;
 			}
+
+			
 		}
 	}
 
@@ -78,11 +92,20 @@
 			width: auto;
 			margin-right: 0;
 			border: none;
+			background-color: inherit;
+
+			flex-direction: column;
+			height: auto;
 			&__mobile-toggle {
 				display: inline-block;
 			}
 			&__link {
-				display: none;
+				&--desktop{
+					display: none;
+				}
+				
+			// position: absolute;
+
 			}
 		}
 	}
