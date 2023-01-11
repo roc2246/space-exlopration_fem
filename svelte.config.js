bae106d06d8dd32b092d6a991e437386479537a1
Riley <riley.childs@yahoo.com>
2023-01-11 14:10:36 -0500

    Undid change to config file

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
 import preprocessor from "svelte-preprocess"
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
